d78ac3ad243fcdc61bcaacd23d475152bd9a859f
SimoIronhacker <keridou4u@yahoo.fr>
2022-08-27 01:56:55 +0200

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -8,6 +8,8 @@ import timeit
 
 
 
+
+
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
@@ -29,54 +31,32 @@ def main():
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
-
                         ''',
                         number=100000,
                         globals=globals()))
     
-    
+
     print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
                         setup=
                         '''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
-
                         ''',
                         number=100000,
                         globals=globals()))
-    
-    fix,ax=plt.subplots()
-    labels= ['Necessary', 'Food', 'Unnecessary']
+
+    fig, ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+
     divided_expenses_sum = []
     for category_exps in divided_set_comp:
-        divided_expenses_sum.append(sum(x.amount for x in category_exps
-))
-    ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
+        divided_expenses_sum.append( sum(x.amount for x in category_exps) )
+
+    ax.pie(divided_expenses_sum, labels=labels, autopct='%1.1f%%')
     plt.show()
 
-        
-    
-    
-    
-    
-    
-    
-    
-    
-            
-            
-    
-
-
-            
-         
-    
-    
-    
-        
-    
-
+   
 
 if __name__ == "__main__":
     main()
